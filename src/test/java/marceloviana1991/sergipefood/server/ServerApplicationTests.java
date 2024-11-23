af190ce48dc64dc6c84b6d5bd1db199c835fd378
marcelo <marceloviana1991@gmail.com>
2024-11-23 17:16:40 -0300

    first commit

---
+++ src/test/java/marceloviana1991/sergipefood/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package marceloviana1991.sergipefood.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
